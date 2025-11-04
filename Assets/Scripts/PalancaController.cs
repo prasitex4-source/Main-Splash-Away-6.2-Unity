752bd5e8709f1e234ef2526a24a5ca34b0fb450b
prasitex4-source <prasitex4@gmail.com>
2025-11-04 18:42:14 +0100

    Nuevos niveles
    1 y 2

--- Assets/Scripts/PalancaController.cs
+++ Assets/Scripts/PalancaController.cs
@@ -3,12 +3,10 @@ using UnityEngine;
 public class PalancaController : MonoBehaviour
 {
     [SerializeField] private GameObject palanca;
-    [SerializeField] private GameObject switched;
-
+        
     private void OnTriggerEnter2D(Collider2D collision)
     {
         palanca.GetComponent<CapsuleCollider2D>().enabled = true;
-        ActivateForm(switched);
-        
+          
     }
 }
