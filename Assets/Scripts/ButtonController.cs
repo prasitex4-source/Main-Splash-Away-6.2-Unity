307814d9f3096388a65c0861c7af4ede31f09be7
dariaivanovna05-create <dariaivanovna05@gmail.com>
2025-11-24 16:43:37 +0100

    build de niveles acabado
    los niveles están maquetados!

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -3,18 +3,34 @@ using UnityEngine;
 public class ButtonController : MonoBehaviour
 {
     [SerializeField] private GameObject activado;
+    [SerializeField] private GameObject desactivado;
     [SerializeField] private Sprite buttonOn;
 
     void Start()
     {
-        activado.SetActive(true);
+        if(activado != null)
+        {
+            activado.SetActive(true);
+        }
+        
+        if(desactivado != null) 
+        {
+            desactivado.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            activado.SetActive(false);
+            if(activado != null)
+            {
+                activado.SetActive(false);
+            }
+            if(desactivado != null)
+            {
+                desactivado.SetActive(true);
+            }
             gameObject.GetComponent<SpriteRenderer>().sprite = buttonOn;
         }
         
