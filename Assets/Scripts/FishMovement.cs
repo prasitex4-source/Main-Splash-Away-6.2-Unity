52c0780122e474d73107c0a787dcbcc3fd725622
prasitex4-source <prasitex4@gmail.com>
2025-12-16 18:51:21 +0100

    cosas

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -14,17 +14,17 @@ public class FishMovement : MonoBehaviour
     [SerializeField] private string waterTag;
 
     [Header("Saltos fuera del agua")]
-    [SerializeField] private float baseJumpForce = 6f;      // Fuerza del primer salto
-    [SerializeField] private float jumpDamping = 0.6f;       // Reducción progresiva de fuerza
-    [SerializeField] private int maxJumps = 3;               // Número exacto de saltos
-    [SerializeField] private float horizontalDamping = 0.98f;// Damping horizontal por frame
-    private Rigidbody2D playerRigidbody2d; //Rigidbody of the player to apply forces and movement.
+    [SerializeField] private float baseJumpForce = 6f;
+    [SerializeField] private float jumpDamping = 0.6f;
+    [SerializeField] private int maxJumps = 3;
+    [SerializeField] private float horizontalDamping = 0.98f;
+    private Rigidbody2D playerRigidbody2d;
 
     [Header("Jump")]
-    [SerializeField] private float jumpForce = 5f; //Force used to jump.
+    [SerializeField] private float jumpForce = 5f;
 
     [Header("Ground check")]
-    [SerializeField] private Transform groundCheckPos;       // Hijo bajo el pez
+    [SerializeField] private Transform groundCheckPos;
     [SerializeField] private Vector2 groundCheckSize = new Vector2(0.4f, 0.05f);
     [SerializeField] private LayerMask groundLayer;
 
@@ -44,16 +44,14 @@ public class FishMovement : MonoBehaviour
     [SerializeField] audiomanager audiomanager;
 
     private bool insideDrop;
-
-
     private Rigidbody2D rb;
     private Vector2 input = Vector2.zero;
     private bool isSwimming = false;
     private bool wasGroundedLastFrame = false;
     public int jumpCount = 0;
     private bool inBucket = false;
-
     private Animator animator;
+    private bool isFacingRight = true;
 
     private void Awake()
     {
@@ -62,12 +60,10 @@ public class FishMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        // Asegúrate de desmarcar Freeze Rotation en Z en el Rigidbody2D
         splashParticle.gameObject.SetActive(false);
         animator = GetComponent<Animator>();
     }
 
-    // Entrada del nuevo Input System
     public void Move(InputAction.CallbackContext context)
     {
         if (context.performed && insideDrop == false)
@@ -89,26 +85,23 @@ public class FishMovement : MonoBehaviour
 
         if (isSwimming)
         {
-            // Movimiento libre dentro del agua
-            rb.gravityScale = 0.1f;
-            jumpCount = 0; // Reinicia saltos al volver al agua
+            rb.gravityScale = 0;
+            jumpCount = 0;
 
             if (Input.GetKey(KeyCode.Space))
             {
                 rb.linearVelocity = input * swimMaxSpeed;
             }
-
             else
             {
                 rb.linearVelocity = input * swimSpeed;
             }
         }
-
         else if (inBucket)
         {
             isSwimming = false;
             transform.position = positionFish.transform.position;
-            playerSprite.localRotation = Quaternion.Euler(0f, 0f, -35.0f);
+            playerSprite.localRotation = Quaternion.Euler(0f, 0f, -35f);
             rb.gravityScale = 0f;
             rb.linearVelocity = Vector2.zero;
             jumpCount = 0;
@@ -118,10 +111,9 @@ public class FishMovement : MonoBehaviour
                 inBucket = false;
                 rb.AddForce(Vector2.up * jumpForce * 1.5f, ForceMode2D.Impulse);
                 bucket.GetComponent<BucketController>().ReactivateCollider();
-                playerSprite.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+                playerSprite.localRotation = Quaternion.Euler(0f, 0f, 0f);
                 SplashPartciles();
             }
-
             else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
                 if (bucket.tag == "derecha" || bucket.tag == "izquierda")
@@ -129,14 +121,10 @@ public class FishMovement : MonoBehaviour
                     inBucket = false;
                     bucket.GetComponent<BucketController>().PushRight();
                     rb.AddForce(Vector2.right * jumpForce, ForceMode2D.Impulse);
-                    playerSprite.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+                    playerSprite.localRotation = Quaternion.Euler(0f, 0f, 0f);
                     SplashPartciles();
                 }
-
-                else { }
-
             }
-
             else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 if (bucket.tag == "derecha" || bucket.tag == "izquierda")
@@ -144,33 +132,25 @@ public class FishMovement : MonoBehaviour
                     inBucket = false;
                     bucket.GetComponent<BucketController>().PushLeft();
                     rb.AddForce(Vector2.left * jumpForce, ForceMode2D.Impulse);
-                    playerSprite.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+                    playerSprite.localRotation = Quaternion.Euler(0f, 0f, 0f);
                     SplashPartciles();
                 }
-
-                else { }
             }
         }
-
         else
         {
             rb.gravityScale = 1f;
             bool grounded = IsGrounded();
 
-
-            // Detecta contacto con el suelo
             if (grounded && !wasGroundedLastFrame)
             {
                 if (jumpCount < maxJumps && !isSwimming)
                 {
                     audiomanager.PlaySFX(audiomanager.jump);
-                    // Resetea la velocidad vertical antes de aplicar salto
-                    rb.linearVelocity = new Vector2(rb.linearVelocityX, 0f);
+                    rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
                     rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-
                     jumpCount++;
                     RotatePlayer();
-  
                 }
                 else
                 {
@@ -180,9 +160,9 @@ public class FishMovement : MonoBehaviour
 
             wasGroundedLastFrame = grounded;
 
-            if (jumpCount < maxJumps)// Damping horizontal configurable
+            if (jumpCount < maxJumps)
             {
-                rb.linearVelocity = new Vector2(swimSpeed * input.x * horizontalDamping, rb.linearVelocityY);
+                rb.linearVelocity = new Vector2(swimSpeed * input.x * horizontalDamping, rb.linearVelocity.y);
             }
         }
     }
@@ -191,8 +171,8 @@ public class FishMovement : MonoBehaviour
     {
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
     }
+
     private void SplashPartciles()
     {
         splashParticle.gameObject.SetActive(false);
@@ -204,11 +184,11 @@ public class FishMovement : MonoBehaviour
         if (other.CompareTag(waterTag))
             isSwimming = true;
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(waterTag) || inBucket)
         {
-            print("Enter Trigger");
             SplashPartciles();
             audiomanager.PlaySFX(audiomanager.splash);
         }
@@ -216,10 +196,8 @@ public class FishMovement : MonoBehaviour
         {
             rb.linearVelocity = Vector2.zero;
             rb.gravityScale = 1f;
-
             audiomanager.PlaySFX(audiomanager.death);
             animator.SetTrigger("dead2");
-
             StartCoroutine(Reiniciar(2.0f));
         }
         else if (collision.tag == "drops")
@@ -227,16 +205,10 @@ public class FishMovement : MonoBehaviour
             jumpCount = 0;
             insideDrop = true;
             transform.SetParent(collision.transform);
-            GetComponent<Rigidbody2D>().linearVelocity = collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity;
-            //StartCoroutine(Reiniciargota());
+            rb.linearVelocity = collision.GetComponent<Rigidbody2D>().linearVelocity;
         }
     }
 
-    private IEnumerator Reiniciargota()
-    {
-        yield return new WaitForSeconds(2f);
-        transform.SetParent(null);
-    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -264,17 +236,12 @@ public class FishMovement : MonoBehaviour
         {
             return;
         }
-
-        Vector2 dir = rb.linearVelocity;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        playerSprite.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
     }
 
     public bool GetBucket()
     {
         return inBucket;
-    } 
-
+    }
 
     public void SetBucket(bool bucket)
     {
@@ -285,20 +252,18 @@ public class FishMovement : MonoBehaviour
     {
         bucket = bck;
     }
+
     public void SetFishPosition(GameObject fishPos)
     {
         positionFish = fishPos;
-    } 
+    }
 
     public void Death()
     {
-
         rb.linearVelocity = Vector2.zero;
         rb.gravityScale = 1f;
-
         audiomanager.PlaySFX(audiomanager.death);
         animator.SetTrigger("dead");
-
         StartCoroutine(Reiniciar(2.0f));
     }
 
